097f867cf5c0d52b7acaea642a2d6638c7f263d0
José Mauro <jmmcarmo@hotmail.com>
2021-11-21 14:26:23 -0300

    Criados os packages , a MainActivity e os fragmentos

---
+++ app/src/main/java/com/example/supermarkethelp/ui/fragment/categories/CategoriesFragment.kt
@@ -0,0 +1,8 @@
+package com.example.supermarkethelp.ui.fragment.categories
+
+import androidx.fragment.app.Fragment
+
+
+class CategoriesFragment : Fragment() {
+}
+
---
+++ app/src/main/java/com/example/supermarkethelp/ui/fragment/categorydetail/CategoryDetailFragment.kt
@@ -0,0 +1,9 @@
+package com.example.supermarkethelp.ui.fragment.categorydetail
+
+
+import androidx.fragment.app.Fragment
+
+
+class CategoryDetailFragment : Fragment() {
+
+}
