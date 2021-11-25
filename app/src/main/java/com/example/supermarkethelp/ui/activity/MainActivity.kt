ed1739fe2a1cdc98512a7a51ca8bff77d22eb263
José Mauro <jmmcarmo@hotmail.com>
2021-11-24 22:08:16 -0300

    Novas Alterações na MainActivity e no Fragmento

--- app/src/main/java/com/example/supermarkethelp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/supermarkethelp/ui/activity/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.supermarkethelp.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.supermarkethelp.R
+import com.example.supermarkethelp.ui.fragment.categories.CategoriesFragment
 
 class MainActivity : AppCompatActivity() {
+    private val manager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        manager.beginTransaction().add(R.id.container_fragment, CategoriesFragment()).commit()
     }
 }
--- app/src/main/java/com/example/supermarkethelp/ui/fragment/categories/CategoriesFragment.kt
+++ app/src/main/java/com/example/supermarkethelp/ui/fragment/categories/CategoriesFragment.kt
@@ -1,8 +1,28 @@
 package com.example.supermarkethelp.ui.fragment.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.supermarkethelp.R
+import com.example.supermarkethelp.ui.activity.MainActivity
 
 
 class CategoriesFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as MainActivity).title = "Principal - Categorias"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       val root = inflater.inflate(R.layout.fragment_categories, container, false)
+        return root
+    }
 }
 
